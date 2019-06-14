7942980c37f98008b20701f0c906501b68c59c5e
Rodrigo Cortes <racortes2@uc.cl>
2019-06-14 17:51:44 -0400

    change pokebal works

--- src/app/components/catch/catch.component.ts
+++ src/app/components/catch/catch.component.ts
@@ -19,8 +19,9 @@ const chance = Chance();
   styleUrls: ['./catch.component.sass']
 })
 export class CatchComponent implements OnInit {
-  pokemon: Pokemon | null = null
+  pokemon: Pokemon | null = null;
   failed: number = 0;
+  isCatching: boolean = false;
 
   constructor(private pokeAPIService: PokeAPIService, private store: Store) { }
 
@@ -28,25 +29,35 @@ export class CatchComponent implements OnInit {
     this.pokeAPIService.getRandomPokemon()
       .subscribe(pokemon => this.pokemon = pokemon);
   }
-
-  catch(pokemon: Pokemon) {
+  
+  continueCatch(pokemon: Pokemon){
     let isCatch = chance.weighted([true, false], [1, 3]);
-    if (pokemon.id === 144 || pokemon.id === 145 || pokemon.id === 146 || pokemon.id === 150 || pokemon.id === 151){
-      isCatch = chance.weighted([true, false], [1, 6]);
-    }
-    if(isCatch){
-      console.log("capturado!");
-      this.failed = 0;
-      this.store.dispatch(new Catch(pokemon));
-      this.pokeAPIService.getRandomPokemon()
-      .subscribe(pokemon => this.pokemon = pokemon);
-    } else{
-      this.failed += 1;
-      console.log("Lo siento :C!");
-      if(this.failed === 3){
-        this.pokeAPIService.getRandomPokemon()
-      .subscribe(pokemon => this.pokemon = pokemon);
+      if (pokemon.id === 144 || pokemon.id === 145 || pokemon.id === 146 || pokemon.id === 150 || pokemon.id === 151){
+        isCatch = chance.weighted([true, false], [1, 6]);
       }
-    }
+      if(isCatch){
+        console.log("capturado!");
+        this.failed = 0;
+        this.store.dispatch(new Catch(pokemon));
+        this.pokeAPIService.getRandomPokemon()
+        .subscribe(pokemon => this.pokemon = pokemon);
+        this.isCatching = false;
+      } else{
+        this.failed += 1;
+        console.log("Lo siento :C!");
+        if(this.failed === 3){
+          this.failed = 0;
+          this.pokeAPIService.getRandomPokemon()
+        .subscribe(pokemon => this.pokemon = pokemon);
+        }
+        this.isCatching = false;
+      }
   }
+
+  startCatch(pokemon: Pokemon) {
+    this.isCatching = true;
+    setTimeout( () => this.continueCatch(pokemon), 3000);
+  
+  }
+
 }
