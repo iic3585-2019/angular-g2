b2974de839701372edb788e64fca727744dd1f55
btkramm <btkramm@uc.cl>
2019-06-13 23:03:44 -0400

    fixed service

--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 // Chance (https://github.com/chancejs/chancejs)
-import Chance from 'chancejs';
+import Chance from 'chance';
 const chance = Chance();
 
 // Types
 import { Pokemon } from '../types/pokemon';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({ providedIn: 'root' })
 export class PokeAPIService {
-  private BASE_URI = 'https://pokeapi.co/api/v2/';
+  private BASE_URI = 'https://pokeapi.co/api/v2';
 
   constructor(private http: HttpClient) { }
 
@@ -19,6 +24,13 @@ export class PokeAPIService {
     const id = chance.integer({ min: 1, max: 151 });
     const uri = `${this.BASE_URI}/pokemon/${id}/`;
 
-    return this.http.get<Pokemon>(uri);
+    const response = this.http.get<any>(uri, httpOptions)
+      .pipe(map(pokemon => ({
+        id,
+        name: pokemon.name,
+        imageURI: pokemon.sprites.front_default,
+      })));
+
+    return response
   }
 }
